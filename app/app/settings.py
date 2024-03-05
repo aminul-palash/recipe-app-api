ecef7508b33e6431bdff1170b93e0b7d5f7684df
aminul.haque <aminul.haque@bjitgroup.com>
2024-03-05 11:52:06 +0600

    database unavialable related issues fixed

--- app/app/settings.py
+++ app/app/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('BD_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 
